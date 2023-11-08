c58e72174a9cff7016da7c2eae3a1bee47b1abf3
Alessandro Cei <ale.steal94@gmail.com>
2023-11-08 23:09:08 +0100

    Navbar now fixes itself to the top once Hero section is scrolled

--- Landing/src/components/NavBar/NavBar.tsx
+++ Landing/src/components/NavBar/NavBar.tsx
@@ -1,11 +1,25 @@
 import './NavBar.css'
 import Logo from '../Logo/Logo'
+import { useState } from 'react';
 
 export default function NavBar() {
-    return (
-      <nav className="navbar">
+
+  const [fixed, setFixed] = useState(false);
+  function fixNavbar() {
+    if (window.scrollY > window.innerHeight) {
+      setFixed(true);
+    } else {
+      setFixed(false);
+    }
+  }
+  window.addEventListener("scroll", fixNavbar)
+
+  return (
+    <>
+      <div className='navbar__placeholder'/>
+      <nav className={fixed ? 'navbar navbar__fixed' : 'navbar'}>
         <div className="navbar__logo">
-            <Logo size={1.1} opacity="100%"/>
+          <Logo size={1.1} opacity="100%"/>
         </div>
         <div className="navbar__links">
           <a href="#">My sauce</a>
@@ -14,5 +28,7 @@ export default function NavBar() {
           <a href="#">Contacts</a>
         </div>
       </nav>
-    )
+    </>
+    
+  )
 }
--- Landing/src/components/PillsGrid/components/PillsRow.tsx
+++ Landing/src/components/PillsGrid/components/PillsRow.tsx
@@ -1,4 +1,5 @@
-import "./PillsRow.css"
+import "./PillsRow.css";
+import { CSSProperties } from "react";
 
 const backgroundColors:string[] = ["#6CE0FF", "#CDA1FF", "#FFA14F", "#B9F08D", "#D9C668"];
 const colors:string[] = ["#0A8DB3", "#8F4B97", "#CC7332", "#7EAA57", "#A5903A"];
@@ -10,16 +11,14 @@ interface PillsRowProps {
 }
 
 export default function PillsGrid(props: PillsRowProps) {
-    const styled = {
-        "--t": props.speed,
-        "--a": "animation" + props.direction,
-        "--a2": "animation" + props.direction + "2"
-    } as React.CSSProperties;
-    return(
 
-        
+    return(
         <div className="pills-row__container">
-            <div className="pills-row__scroll" style={styled}>
+            <div className="pills-row__scroll" style={{
+                "--t": props.speed,
+                "--a": "animation" + props.direction,
+                "--a2": "animation" + props.direction + "2"
+            } as CSSProperties}>
                 <div>
                     {props.technologies.map((element, index) => (
                         <span key={index} className='pill' style={{backgroundColor: backgroundColors[index % colors.length], color: colors[index % colors.length]}}>
@@ -36,6 +35,6 @@ export default function PillsGrid(props: PillsRowProps) {
                 </div>
             </div>
         </div>
-         
     )
+    
 }
