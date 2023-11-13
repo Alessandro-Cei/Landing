a2bf81cd21db58298e61a02fde13e1506a3e8922
Alessandro Cei <ale.steal94@gmail.com>
2023-11-13 13:20:29 +0100

    Added Contacts section's background

--- Landing/src/sections/Contacts/Contacts.tsx
+++ Landing/src/sections/Contacts/Contacts.tsx
@@ -1,6 +1,9 @@
 import SectionTitle from "../../components/SectionTitle/SectionTitle";
 import "./Contacts.css";
-import symbol from "../../assets/contacts.svg"
+import symbol from "../../assets/contacts.svg";
+import baloon1 from "../../assets/baloon1.svg";
+import baloon2 from "../../assets/baloon2.svg";
+import doodle from "../../assets/doodle.svg";
 
 export default function Contacts() {
     function sendMail() {
@@ -8,6 +11,24 @@ export default function Contacts() {
     }
     return(
         <>
+            <div className="contacts__background">
+                <div>
+                    <img src={baloon1} alt=""  style={{
+                        marginRight: "15%",
+                        width: "50%"
+                    }}/>
+                </div>
+                <div>
+                    <img src={baloon2} alt="" style={{
+                        marginLeft: "40%",
+                    }}/>
+                    <img src={doodle} alt="" style={{
+                        filter: "invert(100%) sepia(0%) saturate(7500%) hue-rotate(153deg) brightness(119%) contrast(104%)",
+                        opacity: "0.15",
+                        marginLeft: "40%"
+                    }}/>
+                </div>
+            </div>
             <div className="contacts__container">
                 <div className="contacts__upper-side">
                     <img className="contacts__symbol" src={symbol} alt="contacts section's title symbol" />
