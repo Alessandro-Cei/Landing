09d6fe3af4164d943e8b36b8a57b4ba491068256
Alessandro Cei <ale.steal94@gmail.com>
2023-11-16 14:58:48 +0100

    changed projects images to a fading carousel

--- Landing/src/sections/Projects/Projects.tsx
+++ Landing/src/sections/Projects/Projects.tsx
@@ -1,18 +1,66 @@
 import "./Projects.css";
-import symbol from "../../assets/projects.svg"
-import sakhi1 from "../../assets/sakhi1.png"
-import sakhi2 from "../../assets/sakhi2.png"
-import hakulele1 from "../../assets/hakulele1.png"
-import hakulele2 from "../../assets/hakulele2.png"
+import symbol from "../../assets/projects.svg";
+import barefoot1 from "../../assets/barefoot1.png";
+import barefoot2 from "../../assets/barefoot2.png";
+import barefoot3 from "../../assets/barefoot3.png";
+import hakulele1 from "../../assets/hakulele1.png";
+import hakulele2 from "../../assets/hakulele2.png";
+import hakulele3 from "../../assets/hakulele3.png";
 import SectionTitle from "../../components/SectionTitle/SectionTitle";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import SkillsGrid from "./components/SkillsGrid";
 import BarefootCollege from "./components/BarefootCollege";
 import Hakulele from "./components/Hakulele";
 
 export default function Projects () {
 
-    const [category, setCategory] = useState(0)
+    const [category, setCategory] = useState(0);
+    const [pictures, setPictures] = useState([barefoot1, barefoot2, barefoot3]);
+    const [picturesStyles, setPicturesStyles] = useState(
+        [
+            { maxWidth: 'auto', maxHeight: '100%'},
+            { maxWidth: '100%', maxHeight: 'auto'},
+            { maxWidth: '100%', maxHeight: 'auto'}
+        ]
+    );
+    const [pictureIndex, setPictureIndex] = useState(0);
+    const [fadeOut, setFadeOut] = useState(false);
+
+    useEffect(() => {
+      const intervalId = setInterval(() => {
+        setFadeOut(true);
+        setTimeout(() => {
+          const newIndex = (pictureIndex + 1) % pictures.length;
+          setPictureIndex(newIndex);
+          setFadeOut(false);
+        }, 1000); 
+      }, 4000); 
+  
+      return () => clearInterval(intervalId);
+    }, [pictureIndex, pictures]);
+
+    function changeCategory(num: number) {
+        if (num == 0) {
+            setCategory(0)
+            setPictures([barefoot1, barefoot2, barefoot3])
+            setPictureIndex(0)
+            setPicturesStyles([
+                { maxWidth: 'auto', maxHeight: '100%'},
+                { maxWidth: '100%', maxHeight: 'auto'},
+                { maxWidth: '100%', maxHeight: 'auto'}
+            ])
+        } else {
+            setCategory(1)
+            setPictureIndex(0)
+            setPictures([hakulele1, hakulele2, hakulele3])
+            setPicturesStyles([
+                { maxWidth: 'auto', maxHeight: '100%'},
+                { maxWidth: 'auto', maxHeight: '100%'},
+                { maxWidth: 'auto', maxHeight: '100%'}
+            ])
+        }
+    };
+    
     return(
         <div className="projects__container">
             <div className="projects__upper-side">
@@ -36,13 +84,13 @@ export default function Projects () {
                     <button style={{
                         color: category == 0 ? "#1F1F23" : "#CDA1FF",
                         backgroundColor: category == 0 ? "#CDA1FF" : "#1F1F23"
-                    }} onClick={() => setCategory(0)}>
+                    }} onClick={() => changeCategory(0)}>
                         Barefoot Suite
                     </button>
                     <button style={{
                         color: category == 1 ? "#1F1F23" : "#CDA1FF",
                         backgroundColor: category == 1 ? "#CDA1FF" : "#1F1F23"
-                    }} onClick={() => setCategory(1)}>
+                    }} onClick={() => changeCategory(1)}>
                         Hakulele
                     </button>
                 </nav>
@@ -60,12 +108,11 @@ export default function Projects () {
                                               "On Site Usability Testing", "Animations", "iOS", "Native Components"]
                         }/>
                         <div className="single-project-images">
-                            <img style={{position: "relative", right: "0%", top: "0px", zIndex:"0"}} src={
-                                category == 0 ? sakhi1 : hakulele1
-                            } alt="" />
-                            <img style={{position: "relative", right: "20%", top: "15vh", zIndex:"1"}} src={
-                                category == 0 ? sakhi2 : hakulele2
-                            } alt="" />
+                            <img src={pictures[pictureIndex]} alt={`Image ${pictureIndex + 1}`} style={{
+                                ...picturesStyles[pictureIndex],
+                                opacity: fadeOut ? 0 : 1,
+                                transition: 'opacity 0.5s ease-in-out',
+                            }}/>
                         </div>
                     </div>
                 </div>
