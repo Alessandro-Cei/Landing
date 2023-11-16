b71d9a6d55b3d43c7093cd44e42c517c63380790
Alessandro Cei <ale.steal94@gmail.com>
2023-11-16 18:57:18 +0100

    removed instagram link from footer

--- Landing/src/sections/Footer/Footer.tsx
+++ Landing/src/sections/Footer/Footer.tsx
@@ -12,7 +12,6 @@ export default function Footer() {
                 <div className="footer__info-links">
                     <a href="https://www.linkedin.com/in/alessandro-cei" target="_blank"><img src={linkedin} alt="" /></a>
                     <a href="https://github.com/Alessandro-Cei" target="_blank"><img src={github} alt="" /></a>
-                    <a href="https://www.instagram.com/" target="_blank"><img src={instagram} alt="" /></a>
                     <a href="https://www.behance.net/alessandrocei" target="_blank"><img src={behance} alt="" /></a>
                 </div>
             </div>
