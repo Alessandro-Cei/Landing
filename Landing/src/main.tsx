f29eff18db275dbd66ba847a1548aa5652afec39
Alessandro Cei <ale.steal94@gmail.com>
2023-11-20 22:16:59 +0100

    imported inter and roboto fonts via npm

--- Landing/src/main.tsx
+++ Landing/src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App.tsx'
 import './index.css'
+import '@fontsource-variable/inter';
+import '@fontsource/roboto';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
